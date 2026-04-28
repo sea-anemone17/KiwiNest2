e8b3398e474c0a11c5cc071b30fe1f9cd2ef8653
sea-anemone17 <leehaeju09@gmail.com>
2026-04-28 13:11:33 +0900

    Add Supabase configuration constants

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
+export const SUPABASE_URL = "https://xblubxotbhrocsuwluwj.supabase.co";
 
+export const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhibHVieG90Ymhyb2NzdXdsdXdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMzI2MjUsImV4cCI6MjA5MjkwODYyNX0.M49Y9coD4loCLoreBlY8suuXv735HwSYsTbgTT9ZLtU";
+
+export const USE_SUPABASE = true;
