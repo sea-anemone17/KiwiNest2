f567b0d088d3b49d316f170d8ea9563cfa611457
sea-anemone17 <leehaeju09@gmail.com>
2026-04-28 11:36:38 +0900

    Add files via upload

--- js/data/subjects.js
+++ js/data/subjects.js
@@ -1,1 +1,14 @@
+export const SUBJECTS = [
+  { id: "korean", name: "국어" },
+  { id: "english", name: "영어" },
+  { id: "math", name: "수학" },
+  { id: "law", name: "법률" },
+  { id: "geo", name: "세계지리" },
+  { id: "japanese", name: "일본어" },
+  { id: "info", name: "정보" },
+  { id: "etc", name: "기타" },
+];
 
+export function getSubjectName(id) {
+  return SUBJECTS.find((subject) => subject.id === id)?.name ?? "기타";
+}
