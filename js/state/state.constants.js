8c5d2f47c36fcf28ebc34dfc91cf4df7c05411cc
sea-anemone17 <leehaeju09@gmail.com>
2026-05-03 16:39:37 +0900

    Add files via upload

--- js/state/state.constants.js
+++ js/state/state.constants.js
@@ -1,1 +1,37 @@
+export const STORAGE_KEY = "kiwinest-alpha-state-v1";
 
+export const CURRENT_SCHEMA_VERSION = 7;
+
+export const DEFAULT_STATE = {
+  schemaVersion: CURRENT_SCHEMA_VERSION,
+  kiwi: {
+    name: "위키",
+    exp: 0,
+    affection: 0,
+    titleTickets: 0,
+  },
+  kiwiDex: {
+    selectedVariantId: "basic",
+    unlockedVariantIds: ["basic"],
+    newlyUnlockedIds: [],
+  },
+  titles: {
+    equippedTitleId: "nest_beginner",
+    ownedTitleIds: ["nest_beginner"],
+    recentTitleIds: [],
+  },
+  achievements: {
+    unlockedIds: [],
+    recentIds: [],
+  },
+  letters: {
+    unlockedIds: [],
+    recentIds: [],
+  },
+  diaries: [],
+  metaSessions: [],
+  calmLogs: [],
+  reviewQueue: [],
+  customSubjects: [],
+  lastMessage: "둥지에 온 걸 환영해요.\n오늘은 무엇을 가르쳐 줄 건가요?",
+};
